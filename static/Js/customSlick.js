218af3d906d66cabf469889b3cc17f4e9d08892c
Nguyễn Văn Thuần <165972755+vthuan-dev@users.noreply.github.com>
2025-04-21 18:06:09 +0700

    temp ccss

--- static/Js/customSlick.js
+++ static/Js/customSlick.js
@@ -1,30 +1,26 @@
 $(document).ready(function () {
   $(".book-slider").slick({
-    slidesToShow: 5,
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 4000,
     dots: false,
     arrows: true,
+    infinite: true,
+    speed: 500,
+    cssEase: 'ease-out',
+    centerPadding: '60px',
     responsive: [
       {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: false,
-        },
-      },
-      {
-        breakpoint: 600,
+        breakpoint: 1200,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
+          infinite: true,
         },
       },
       {
-        breakpoint: 480,
+        breakpoint: 768,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
