0828bc765fb0aa2fb1933e723f49547902f4f622
Kokie <thaithuan2407@gmail.com>
2025-04-24 10:52:11 +0700

    fix bug

--- home/templatetags/custom_filters.py
+++ home/templatetags/custom_filters.py
@@ -3,10 +3,19 @@ from django import template
 register = template.Library()
 
 @register.filter
-def get_item(dictionary, key):
+def get_number(dictionary, key):
+    """
+    Returns numeric value from dictionary with fallback to 0
+    Used for ratings and counts
+    """
     if not dictionary:
         return 0
     try:
         return dictionary.get(str(key), 0)  # Convert key to string
     except (AttributeError, TypeError):
         return 0
+
+@register.filter
+def subtract(value, arg):
+    return value - arg
+
--- home/templatetags/filters.py
+++ home/templatetags/filters.py
@@ -3,10 +3,14 @@ from django import template
 register = template.Library()
 
 @register.filter
-def subtract(value, arg):
-    return value - arg
-
-@register.filter
 def get_item(dictionary, key):
+    """
+    Returns string value from dictionary
+    Used for text content
+    """
     value = dictionary.get(key)
     return str(value)
+
+@register.filter
+def subtract(value, arg):
+    return value - arg
