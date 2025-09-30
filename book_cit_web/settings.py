10b2c9c4124619bf2d7c36cdddd1760f4a7363e5
Kokie <thaithuan2407@gmail.com>
2025-09-30 09:54:46 +0700

    update staticfiles

--- book_cit_web/settings.py
+++ book_cit_web/settings.py
@@ -162,7 +162,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
