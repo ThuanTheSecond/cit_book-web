cfa401c0dadbfe289879f6924ccc68dc2bcf7313
Kokie <thaithuan2407@gmail.com>
2025-09-30 15:31:12 +0700

    add cloudinary

--- book_cit_web/settings.py
+++ book_cit_web/settings.py
@@ -53,6 +53,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
     'home',
     'account',
     'widget_tweaks',
